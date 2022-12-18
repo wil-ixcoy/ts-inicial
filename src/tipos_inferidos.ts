5d0abc9cdf529b6f4278080abeca9f51582643a1
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-12-17 19:59:09 -0600

    vista de los tipos inferidos en typescript, es decir que detecta que tipo de dato es solo con lo que se asigne

---
+++ src/tipos_inferidos.ts
@@ -0,0 +1,9 @@
+let myProductName:string = "sopas";
+myProductName = "Jugos";
+myProductName.toLowerCase();
+
+let myProductPrice:number = 23;
+myProductPrice.toFixed();
+
+const myCategory = "Lacteos";
+
