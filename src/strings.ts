f20ca494c6ea11feb6781fab367cb45ee54d8851
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-12-17 21:46:24 -0600

    string types

---
+++ src/strings.ts
@@ -0,0 +1,8 @@
+let nombre = "Wiliams Alexander";
+
+let nombreDefinido: string = "Wiliams";
+
+console.log("nombre definido:", nombreDefinido);
+
+let backtics = `Mi nombre es ${nombreDefinido} y soy de Guatemala.`
+console.log("Texto con backtics: ", backtics);
