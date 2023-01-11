40fedee1a9378c71be3c3fd93009604b10a9839a
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-01-11 17:38:29 -0600

    type any

---
+++ src/any.ts
@@ -0,0 +1,13 @@
+/* es recomendable no usar el any */
+
+/* any significa cualquiera, por lo que es lo mismo que js */
+
+let variable : any;
+
+/* el any se puede usar cuando no se sabe que dato viene de una
+funcion, por eso con any se espera cualquier cosa */
+
+variable = "hola";
+variable = 23;
+variable = {};
+variable = null;
